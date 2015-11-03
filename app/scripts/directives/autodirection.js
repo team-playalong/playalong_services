0b1078a10ac687d381f3e20e397c028c67ede9d4
Dadi Atar <atardadi@gmail.com>
2015-11-04 00:39:21 +0200

    auto direction directive

---
+++ app/scripts/directives/autodirection.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name playalongservicesApp.directive:autoDirection
+ * @description
+ * # autoDirection
+ */
+/*jshint unused:false*/
+angular.module('playalong.services')
+  .directive('autoDirection',['$interval', function ($interval) {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        // $interval(function() {
+        // 	var text = element.text();
+
+        // },1000, 0 /*infinity*/,false /*no apply*/);
+
+
+        function checkRTL(s){           
+          var ltrChars    = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF',
+              rtlChars    = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC',
+              rtlDirCheck = new RegExp('^[^'+ltrChars+']*['+rtlChars+']');
+
+          return rtlDirCheck.test(s);
+        }
+
+        // BIND KEYPRESS
+        function keypress(e){
+            // need to wait for the character
+            if (e.charCode === 32) {return;}
+            setTimeout(function(){
+                var isRTL = checkRTL( String.fromCharCode(e.charCode) ),
+                    dir = isRTL ? 'RTL' : 'LTR';
+                
+                element.css('direction', dir);
+            },0);
+        }
+        var input = element.on('keypress', keypress)[0];
+      }
+    };
+  }]);
